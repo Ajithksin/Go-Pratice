522ad2431fa2d4b8940cbcc36dcfa166627faea0
“ajit <ajithkumar.sinha@srsconsultinginc.com>
2022-06-24 06:38:25 +0000

    new func

---
+++ Ajit/jsonH_test.go
@@ -0,0 +1,30 @@
+package ajit
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestEmployeeSalary(t *testing.T) {
+	tests := []struct {
+		name string
+		//want  *os.File
+		want1 bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:  "testing the file size",
+			want1: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, got1 := EmployeeSalary()
+
+			if got1 != tt.want1 {
+				t.Errorf("EmployeeSalary() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
