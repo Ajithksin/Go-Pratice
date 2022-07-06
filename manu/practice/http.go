901c589fff07d01f3f7cc5ae94437a9ba5da9fb5
Manu hali <manu.vijaykumar@srsconsultinginc.com>
2022-07-06 12:10:49 +0000

    adding sample http server

--- manu/practice/http.go
+++ manu/practice/http.go
@@ -1,29 +1,13 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"net/http"
-// )
-
-// func main() {
-// 	http.HandleFunc("/Home", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintf(w, "Welcome to Golang")
-
-// 		http.HandleFunc("/Gadgets", func(w http.ResponseWriter, r *http.Request) {
-// 			fmt.Fprintf(w, "Mobiles, cameras, laptops")
-// 		})
-// 		http.ListenAndServe(":5050", nil)
-// 	})
-// }
-
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
+
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to golang server!")
 	})
@@ -34,8 +18,10 @@ func main() {
 
 	http.HandleFunc("/introduction", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Get set GO!!!!")
-		
 	})
 
+	http.HandleFunc("/time&date", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Today is:", time.Now())
+	})
 	http.ListenAndServe(":5050", nil)
 }
